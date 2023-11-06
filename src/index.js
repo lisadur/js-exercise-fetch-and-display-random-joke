4f3180847edb4617ceb5cacd69c219d4165b6cf2
Lisa Durdun <lisa.durdun@gmail.com>
2023-11-06 17:58:01 +0100

    solved

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 function fetchJokes() {
-  // code here :
+  return fetch("https://official-joke-api.appspot.com/random_joke")
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
 }
 
 async function displayJokes() {
-  // code here :
+  try {
+    const jokeContainer = document.getElementById("joke-container");
+    const jokeSetup = document.createElement("p");
+    const firstjokes = document.createElement("p");
+    const jokeData = await fetchJokes();
+
+    jokeSetup.innerHTML = jokeData.setup;
+    firstjokes.innerHTML = jokeData.punchline;
+    jokeContainer.appendChild(jokeSetup);
+    setTimeout(() => jokeContainer.appendChild(firstjokes), 2000);
+  } catch (error) {
+    console.log("Error displaying jokes:", error);
+  }
 }
 
-// don't forget to invoke the function :
-// displayJokes()
+displayJokes();
